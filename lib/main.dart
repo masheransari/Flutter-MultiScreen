ab0b2ecf38214f6dceddf9bc439daf747d1b87a1
Muhammad Asher Ansari <asher.ansari@cloudasset.com>
2021-09-22 16:46:16 +0500

    remove app bar

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,6 @@ void main() => runApp(myApp());
 class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "My First App",
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Multi Screen App"),
-          backgroundColor: Colors.amber,
-        ),
-        body: cont(),
-      ),
-    );
+    return cont();
   }
 }

7759b09bb9db686dbaf5ee297bc6141af466b369
Muhammad Asher Ansari <asher.ansari@cloudasset.com>
2021-09-22 17:13:52 +0500

    Few basic styling.

--- lib/screens/second_screen.dart
+++ lib/screens/second_screen.dart
@@ -3,13 +3,24 @@ import 'package:flutter/material.dart';
 class cont extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Center(
+        child: Container(
       alignment: Alignment.center,
       color: Colors.blue,
-      child: Text(
-        "Cont Class",
-        textDirection: TextDirection.ltr,
+      child: Center(
+        child: Text(
+          "Cont Class",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+              decoration: TextDecoration.none,
+              fontSize: 40.0,
+              fontFamily: 'Mansalva',
+              color: Colors.amber),
+        ),
       ),
-    );
+      margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0, bottom: 20.0),
+      padding:
+          EdgeInsets.only(left: 10.0, right: 20.0, top: 30.0, bottom: 50.0),
+    ));
   }
 }
